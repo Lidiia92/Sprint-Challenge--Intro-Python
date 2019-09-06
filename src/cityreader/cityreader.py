f4ac34496870811c7208d46e6998c57db2662a14
Lidii <lidiiany92@gmail.com>
2019-09-06 08:27:47 -0700

    Cities assignment

--- src/cityreader/cityreader.py
+++ src/cityreader/cityreader.py
@@ -14,12 +14,30 @@
 #
 # Note that the first line of the CSV is header that describes the fields--this
 # should not be loaded into a City object.
+
+import csv
+
+class City:
+    def __init__(self, name, lat, lon):
+        self.name = name
+        self.lat = lat
+        self.lon = lon
+
+    def __str__(self):
+        return f"{self.name}, {self.lat}, {self.lon}"
+
 cities = []
 
 def cityreader(cities=[]):
   # TODO Implement the functionality to read from the 'cities.csv' file
   # For each city record, create a new City instance and add it to the 
   # `cities` list
+
+  with open('c:\\Users\\Lidii\\Desktop\\LAMBDA\\CS\\Week 01\\Sprint-Challenge--Intro-Python\\src\\cityreader\\cities.csv', newline='') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+      city = City(row['city'], float(row['lat']), float(row['lng']))
+      cities.append(city)
     
     return cities
 
